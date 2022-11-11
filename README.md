83ba60dda71e3b2d8516a97f02f8019b723006b5
Tibor Tálosi <teeebor@gmail.com>
2022-11-11 10:37:02 +0100

    new build script blackjack should be now compatible with the latest release

--- README.md
+++ README.md
@@ -12,25 +12,29 @@ Place the blackjack.fap file on your SD card in the apps folder
 ![Welcome screen](screenshots/play_scene.png)
 
 ![Welcome screen](screenshots/game_over_scene.png)
-## Building
 
+## Build using the build.sh script
+* Clone the repository recursively `git clone REPO_URL --recursive`
+  * if needed checkout a different release for the firmwares inside the firmware folder, I'll try to keep the up-to-date
+* Make sure that the build script has the executable flag set
+* build all the applications with `./build {firmware}`
+  * accepted firmware types are: 
+    * unleashed
+    * vanilla
+    * all
+
+## Custom build
 > The app should be compatible with the official and custom flipper firmwares. If not, follow these steps to build it
 > yourself
-
 * Download your firmware's source code
-* Put the blackjack folder inside the firmware's applications_user folder
+* Copy the common directory inside the chosen game's folder
+* Put the chosen application folder inside the firmware's applications_user folder
 * Make sure you can use
   the [Fipper build tool](https://github.com/flipperdevices/flipperzero-firmware/blob/dev/documentation/fbt.md)
-  * open a console, navigate inside the firmware folder, then run:
-    #### on Linux
-    > ./fbt
-    #### on Windows
-    > fbt.cmd
-
 * To build the project, type this into your console:
   #### Linux
-  > ./fbt fap_blackjack
+  > ./fbt fap_{APP_NAME}
   #### Windows
-  > fbt.cmd fap_blackjack
+  > fbt.cmd fap_{APP_NAME}
 * the finished build will be in the following location, copy this into your SD card:
   > build\f7-firmware-D\.extapps\blackjack.fap
--- build.sh
+++ build.sh
@@ -15,27 +15,56 @@ if [ "$1" = "all" ]; then
 fi
 CURR_DIR=$PWD
 
+ck_dir_notexists () {
+    for dir do
+        [ ! -d "$dir" ] || return 1
+    done
+}
+
+#modified fbt.sh to allow using github submodules
+build_app(){
+  orig_path=$PATH;
+  set -eu;
+  SCRIPT_PATH="$(cd "firmware/$1" && pwd -P)";
+  cd $SCRIPT_PATH
+  SCONS_DEFAULT_FLAGS="-Q --warn=target-not-built";
+  SCONS_EP="python3 -m SCons"
+  FBT_NOENV="${FBT_NOENV:-""}";
+  FBT_NO_SYNC="${FBT_NO_SYNC:-""}";
+  FBT_TOOLCHAIN_PATH="${FBT_TOOLCHAIN_PATH:-$SCRIPT_PATH}";
+  if [ -z "$FBT_NOENV" ]; then
+      . "$SCRIPT_PATH/scripts/toolchain/fbtenv.sh";
+  fi
+  if [ -z "$FBT_NO_SYNC" ]; then
+      if ck_dir_notexists "$SCRIPT_PATH/.git" "$SCRIPT_PATH/.github"; then
+          echo "\".git\" directory not found, please clone repo via \"git clone --recursive\"";
+          exit 1;
+      fi
+      git submodule update --init;
+  fi
+  $SCONS_EP $SCONS_DEFAULT_FLAGS "$2"
+  cd $CURR_DIR
+  PATH=$orig_path
+  PS1=""
+  PROMPT=""
+}
+
 for f in blackjack solitaire; do
   if [ "$UNLEASHED" -gt 0 ]; then
-    echo "Building app '$f' for unleashed"
+    echo "\n\n\e[0;32m Building app '$f' for unleashed\e[0m\n\n"
     rm -r -f firmware/unleashed/applications_user/$f
     cp -R $f firmware/unleashed/applications_user/$f
     cp -R common firmware/unleashed/applications_user/$f/common
-    cd firmware/unleashed
-    ./fbt fap_$f
-    cp build/f7-firmware-D/.extapps/$f.fap $CURR_DIR/builds/unleashed_$f.fap
-    cd $CURR_DIR
+
+    build_app unleashed fap_$f
   fi
   if [ "$VANILLA" -gt 0 ]; then
-    echo "Building app '$f' for vanilla"
+    echo "\n\n\e[0;32mBuilding app '$f' for vanilla\e[0m\n\n"
 
     rm -r -f firmware/official/applications_user/$f
     cp -R $f firmware/official/applications_user/$f
     cp -R common firmware/official/applications_user/$f/common
-    cd firmware/official
-    ./fbt fap_$f
-    cp build/f7-firmware-D/.extapps/$f.fap $CURR_DIR/builds/vanilla_$f.fap
-    cd $CURR_DIR
+    build_app official fap_$f
   fi
 done
 cd
